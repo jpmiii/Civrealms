b98291bbfb4dc7ade141cf47b331ba195d330698
Jim McCall <gitid@jpmiii.com>
2014-04-20 11:35:22 -0500

    latest changes

--- src/com/jpmiii/Civrealms/Civrealms.java
+++ src/com/jpmiii/Civrealms/Civrealms.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import org.bukkit.Bukkit;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -24,6 +25,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.inventory.ItemStack;
 //import org.bukkit.inventory.ItemStack;
 //import org.bukkit.inventory.Recipe;
@@ -39,6 +41,7 @@ public class Civrealms extends JavaPlugin implements Listener {
 	public Permission perms = null;
 	public Map<String, Object> jailList = new HashMap<String, Object>();
 	public Map<String, Object> jailPlayers = new HashMap<String, Object>();
+	BukkitTask gentask = null;
 	public void onEnable() {
 		// getLogger().info("onEnable has been invoked!");
 
@@ -49,6 +52,8 @@ public class Civrealms extends JavaPlugin implements Listener {
 				this, this);
 		setupPermissions();
 		BukkitTask t = new CivrealmsTask(this).runTaskTimer(this, 6000, 6000);
+		
+		
 
 /*
 
@@ -81,7 +86,7 @@ public class Civrealms extends JavaPlugin implements Listener {
 
 		getLogger().info("onDisable has been invoked!");
 	}
-	/*
+	
 
 	@EventHandler(priority = EventPriority.HIGH)
 	public void jail(EntityDamageByEntityEvent event) {
@@ -89,63 +94,111 @@ public class Civrealms extends JavaPlugin implements Listener {
 				&& (event.getDamager().getType() == EntityType.PLAYER)
 				&& ((((Damageable) event.getEntity()).getHealth() - event
 						.getDamage()) < 0)) {
-			if (((Player)event.getDamager()).hasPermission("civ.jail")) {
+			//this.getLogger().info("deathblow");
+			if ((((Player)event.getDamager()).hasPermission("civ.jail") && !event.getDamager().getWorld().getName().equalsIgnoreCase("world_the_end") )) {
+				//this.getLogger().info("ptest");
 
 				ItemStack inhand = ((Player) event.getDamager()).getItemInHand();
 				if (inhand.hasItemMeta()) {
-					String iname = inhand.getItemMeta().getDisplayName();
+					String iname = inhand.getItemMeta().getDisplayName().toLowerCase();
+					//this.getLogger().info(iname);
 					Location jloc = null;
 					if (jailList.containsKey(iname)) {
+						//this.getLogger().info(iname + ": found");
 						String[] locl = ((String)jailList.get(iname)).split(",");
 						if (Integer.parseInt(locl[4]) >= this.getConfig().getInt("votes")) {
+							this.getLogger().info(((Player) event.getEntity()).getName() +"  Jailed in  "+ iname);
 
 
-						jloc = new Location(this.getServer().getWorld(locl[0]),Float.parseFloat(locl[1]), Float.parseFloat(locl[2]), Float.parseFloat(locl[3]));
-						event.setCancelled(true);
-						((Damageable) event.getEntity()).setHealth(20.0);
-						for (ItemStack is : ((Player) event.getEntity()).getInventory().getContents()){
-							if (is != null) {
-								if (is.getType() != Material.AIR) event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), is);
+							jloc = new Location(this.getServer().getWorld(locl[0]),Float.parseFloat(locl[1]), Float.parseFloat(locl[2]), Float.parseFloat(locl[3]));
+							event.setCancelled(true);
+							((Damageable) event.getEntity()).setHealth(20.0);
+							for (ItemStack is : ((Player) event.getEntity()).getInventory().getContents()){
+								if (is != null) {
+									if (is.getType() != Material.AIR) event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), is);
+								}
 							}
-						}
-						for (ItemStack is2 : ((Player) event.getEntity()).getInventory().getArmorContents()){
-							if (is2 != null) {
-								if (is2.getType() != Material.AIR) event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), is2);
+							for (ItemStack is2 : ((Player) event.getEntity()).getInventory().getArmorContents()){
+								if (is2 != null) {
+									if (is2.getType() != Material.AIR) event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), is2);
+								}
 							}
-						}
-						((Player) event.getEntity()).getInventory().clear();
-						((Player) event.getEntity()).getInventory().setArmorContents(null);
-						((Player) event.getEntity()).setBedSpawnLocation(jloc, true);
+							((Player) event.getEntity()).getInventory().clear();
+							((Player) event.getEntity()).getInventory().setArmorContents(null);
+							((Player) event.getEntity()).setBedSpawnLocation(jloc, true);
 
 
 
-						event.getEntity().teleport(jloc);
+							event.getEntity().teleport(jloc);
 
 
-					}}
+						}}
 				}
 			}
 		}
-	} */
+	} 
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void orefix(ChunkLoadEvent event) {
+		
+		if (event.getWorld().getName().equalsIgnoreCase("world") && (event.getChunk().getZ() > -218)) {
+			if (!event.isNewChunk()) {
+				if (event.getChunk().getBlock(1, 6, 2).getType() != Material.BEDROCK) {
+					// makeVein(Chunk, lowest_y, highest_y, blocks_in_vein, Material)
+
+					this.makeVein(event.getChunk(), 7, 15, ((int)(Math.random() * 3 + 1)), Material.DIAMOND_ORE);
+					this.makeVein(event.getChunk(), 7, 47, ((int)(Math.random() * 8 + 1)), Material.GOLD_ORE);
+					this.makeVein(event.getChunk(), 20, 40, ((int)(Math.random() * 5 + 1)), Material.LAPIS_ORE);
+					this.makeVein(event.getChunk(), 7, 16, ((int)(Math.random() * 3 + 1)), Material.LAPIS_ORE);
+
+					event.getChunk().getBlock(1,6,2).setType(Material.BEDROCK);				
+				}
+			}
+		}
+		if (event.getWorld().getName().equalsIgnoreCase("world_nether")) {
+			if (!event.isNewChunk()) {
+				if (event.getChunk().getBlock(1, 6, 2).getType() == Material.BEDROCK) {
+					for  (int x = 0; x <16; x++) {
+						for (int y = 7; y < 47; y++) {
+							for (int z = 0; z < 16; z++) {
+								if ((event.getChunk().getBlock(x, y, z).getType() == Material.DIAMOND_ORE) || (event.getChunk().getBlock(x, y, z).getType() == Material.GOLD_ORE) || (event.getChunk().getBlock(x, y, z).getType() == Material.LAPIS_ORE)) {
+									event.getChunk().getBlock(x, y, z).setType(Material.AIR);
+								}
+							}
+						}
+					}
+
+					event.getChunk().getBlock(1,6,2).setType(Material.NETHER_BRICK);				
+				}
+			}
+		}
+
+	}
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void spawnmove(PlayerRespawnEvent event) {
 		if (!event.isBedSpawn()) {
 			Location curloc = event.getRespawnLocation();
 			boolean look =true;
+			int lookcount = 0;
 			while (look) {
 				World wld = getServer().getWorld(
 						this.getConfig().getString("worldName"));
-				Integer xloc = ((int) (Math.random() * 190 - 100 + curloc
+				Integer xloc = ((int) (Math.random() * 200 - 100 + curloc
 						.getX()));
-				Integer zloc = ((int) (Math.random() * 190 - 100 + curloc
+				Integer zloc = ((int) (Math.random() * 200 - 100 + curloc
 						.getZ()));
 				Integer yloc = wld.getHighestBlockYAt(xloc, zloc);
-				if (wld.getBlockAt(xloc, yloc-1, zloc).getType() == Material.GRASS) {
+				if ((wld.getBlockAt(xloc, (yloc-1), zloc).getType() == Material.GRASS) || (wld.getBlockAt(xloc, (yloc-1), zloc).getType() == Material.SAND)) {
 					look = false;
 					wld.setSpawnLocation(xloc, yloc, zloc);
-					this.getLogger().info("spawn moved");
+					this.getLogger().info("spawn moved " + xloc.toString() + ":" + yloc.toString() + ":" +zloc.toString());
+				}
+				lookcount ++;
+				if (lookcount > 100) {
+					this.getLogger().info("spawn not moved");
+					look = false;
 				}
 			}
+			
 
 		}
 	}
@@ -159,8 +212,8 @@ public class Civrealms extends JavaPlugin implements Listener {
 			}	
 			Player player = (Player) sender;
 			if (args.length > 0) {
-	/*			if (jailList.containsKey(args[0])) {
-					jailPlayers.put(player.getName(), args[0]);
+				if (jailList.containsKey(args[0].toLowerCase())) {
+					jailPlayers.put(player.getName(), args[0].toLowerCase());
 					
 					String Rem = "no";
 
@@ -190,14 +243,14 @@ public class Civrealms extends JavaPlugin implements Listener {
 					this.getConfig().createSection("jailPlayers", jailPlayers);
 					this.saveConfig();
 					//this.getLogger().info(args[0]);
-					player.sendMessage("voted for jail: " + args[0]);
+					player.sendMessage("voted for jail: " + args[0].toLowerCase());
 					return true;
 					
 
 
 				} else {
-					jailPlayers.put(player.getName(), args[0]);
-					jailList.put(args[0], player.getWorld().getName() +"," + player.getLocation().getBlockX() + "," + player.getLocation().getBlockY() +","+ player.getLocation().getBlockZ() + ",1");
+					jailPlayers.put(player.getName(), args[0].toLowerCase());
+					jailList.put(args[0].toLowerCase(), player.getWorld().getName() +"," + player.getLocation().getBlockX() + "," + player.getLocation().getBlockY() +","+ player.getLocation().getBlockZ() + ",1");
 
 					for (String jailname : jailList.keySet()) {
 						int vcount = 0;
@@ -217,11 +270,11 @@ public class Civrealms extends JavaPlugin implements Listener {
 					this.getConfig().createSection("jailList", jailList);
 					this.getConfig().createSection("jailPlayers", jailPlayers);
 					this.saveConfig();
-					this.getLogger().info(args[0]);
-					player.sendMessage("added jail " + args[0]);
+					this.getLogger().info("added jail " + args[0].toLowerCase());
+					player.sendMessage("added jail " + args[0].toLowerCase());
 					return true;
 
-				}*/
+				}
 			}
 
 		}
@@ -281,11 +334,81 @@ public class Civrealms extends JavaPlugin implements Listener {
 					return true;
 				}
 
+
 			}
 
 		}
 
 		return false;
+	}
+	private int[] startVein(Chunk c, int bot, int top) {
+		int v[] = new int[]{-1,-1,-1};
+		int tries = 0;
+		while (tries < 100) {
+			tries ++;
+			int xa = (int)(Math.random() * 16);
+			int za = (int)(Math.random() * 16);
+			int ya = (int)((Math.random() * (top-bot+1))+bot);
+			if (c.getBlock(xa, ya, za).getType() == Material.STONE || c.getBlock(xa, ya, za).getType() == Material.AIR) {
+				v[0] = xa;
+				v[1] = ya;
+				v[2] = za;
+				tries = 111;
+
+			}
+
+		}
+
+		return v;
+
+	}
+	
+
+		
+		
+	
+	private int makeVein(Chunk c, int bottom, int top, int num, Material typ) {
+		int[] v = this.startVein(c, bottom, top);
+		int tot = 0;
+		if (v[0] >= 0){
+			tot=1;
+			int tries = 0;
+			c.getBlock(v[0],v[1],v[2]).setType(typ);
+			while ((tot < num) && (tries < 100)) {
+				tries++;
+				int[] v2 = this.randMove(v);
+				if (((c.getBlock(v2[0],v2[1],v2[2]).getType() == Material.STONE) || (c.getBlock(v2[0],v2[1],v2[2]).getType() == Material.AIR)) && (v2[0] >= 0) && (v2[1] >= bottom) && (v2[2] >= 0) && (v2[0] < 16) && (v2[1] <= top) && (v2[2] < 16)) {
+					c.getBlock(v2[0],v2[1],v2[2]).setType(typ);
+					tot++;
+				} else {
+					v = this.randMove(v);
+				}
+				
+				
+			}
+
+		}
+		
+		return tot;
+		
+	}
+	private int[] randMove(int[] v) {
+		int d = 0;
+		if (Math.random() > 0.5) {
+			d = 1;
+		} else {
+			d = -1;
+		}
+		double rnd = Math.random();
+		if (rnd < 0.33) {
+			v[0] = v[0] + d;
+		} else if (rnd < 0.66) {
+			v[1] = v[1] + d;
+		} else {
+			v[2] = v[2] + d;
+		}
+		return v;
+		
 	}
 
 }
